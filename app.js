92fff54b5d1a411937acee0a6ee1a9170bacb5a0
OLUSEGUN STEPHEN AYOOLUWA <olusegunstephen222@gmail.com>
2025-04-15 16:42:28 +0100

    fixing cors

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
-      "http://192.168.208.198:3000",
-      "https://allegedly-related-jay.ngrok-free.app",
+      // "http://localhost:3000",
+      // "http://192.168.208.198:3000",
+      // "https://allegedly-related-jay.ngrok-free.app",
       "https://devhiretalents.netlify.app",
       "https://devhiretalents.vercel.app",
       "https://dev-hire-pi.vercel.app",
